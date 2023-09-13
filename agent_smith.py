0ae52bd33ecd42ab2a5a2a67231986fc322c3aa6
frankzl <frankz.lu@googlemail.com>
2023-09-13 23:08:19 +0200

    remove prints

--- agent_smith.py
+++ agent_smith.py
@@ -6,10 +6,6 @@ class AgentSmith:
     def compute_action(self, marker: chr, state: State) -> Action:
         for y in range(0,state.rows):
             for x in range(0,state.cols):
-                print(state.rows,state.cols)
-                print(x,y,)
-                print(state.read(x,y))
                 if state.read(x,y) == " ":
-                    print("doing", x,y)
                     return Action(x,y)
 
